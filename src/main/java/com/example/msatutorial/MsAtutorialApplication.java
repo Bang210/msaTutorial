b7469f0325f67e60166ab31adc98e9fed50b6305
Bang210 <bangvelope@gmail.com>
2024-06-03 17:50:45 +0900

    gateway init

---
+++ src/main/java/com/example/msatutorial/MsAtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.msatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAtutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAtutorialApplication.class, args);
+    }
+
+}
